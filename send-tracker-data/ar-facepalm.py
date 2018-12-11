aabfe2c89f4ad1d76a68dccea39c0786f4200968
Charles Holbrow <CharlesHolbrow@gmail.com>
2018-12-11 16:29:43 -0500

    python script sends controller data

--- send-tracker-data/ar-facepalm.py
+++ send-tracker-data/ar-facepalm.py
@@ -30,26 +30,40 @@ if interval:
         # Get the tracker values
         try:
             tracker_pose = v.devices["tracker_1"].get_pose_quaternion()
+            for each in tracker_pose:
+                txt += "%.4f " % each
+            try:
+                client.send_message("/pos", tracker_pose) # send
+            except socket.error as e:
+                print("Socket Error when sending /pos osc:", e)
         except:
             print("failed to get tracker pose")
-        for each in tracker_pose:
-            txt += "%.4f " % each
-        try:
-            client.send_message("/pos", tracker_pose) # send
-        except socket.error as e:
-            print("Socket Error when sending osc:", e)
+            txt += "failed to get tracker pose - "
 
         # Get the Controllers if they exist:
+        txt += " --- "
         try:
+            controller = controller_pose = v.devices["controller_1"]
             controller_pose = v.devices["controller_1"].get_pose_quaternion()
-        except:
-            print("failed to get controller pose")
-        for each in controller_pose:
-            txt += "%.4f " % each
-        if not printed:
-            try:
-                print(dir(v.devices["controller_1"]))
+            # I believe controller.index should be the argument to
+            # getControllerState, but I'm not totally sure.
+            # controller_state = v.vr.getControllerState(controller.index)[1] # this call ruins everything
+            # trigger = controller_state.rAxis[1].x
+            # pad_x = controller_state.rAxis[0].x
+            # pad_y = controller_state.rAxis[0].y
+            # controller_pose.append(trigger)
+            if not printed:
                 printed = True
+                print(dir(controller))
+            try:
+                client.send_message("/controller", controller_pose) # send
+            except socket.error as e:
+                print("Socket Error when sending /controller osc:", e)
+            for each in controller_pose:
+                txt += "%.4f " % each
+        except:
+            print('Failed to get controller pose')
+            txt += "failed to get Controller Pose"
 
         print("\r" + txt, end="")
         sleep_time = interval-(time.time()-start)
