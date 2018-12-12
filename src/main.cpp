94e47a69da76aaf62adb8c54bbac0a772bb47446
Charles Holbrow <CharlesHolbrow@gmail.com>
2018-12-12 17:25:52 -0500

    a bunch of window options

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1280,720,OF_WINDOW);			// <-------- setup the GL context
+//    ofSetupOpenGL(1920,1080,OF_WINDOW);
+//    ofSetupOpenGL(1600,900,OF_WINDOW);
+//    ofSetupOpenGL(1280,720,OF_WINDOW);
+    ofSetupOpenGL(1024, 576,OF_WINDOW); // fov = 42.5
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
